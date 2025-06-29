e3ada2937146f47a3088e417f0590a416d099044
brucechan0824 <brucechan0824@gmail.com>
2025-06-29 21:18:57 +0800

    Add Google Analytics tracking code (G-JEYXGQKYNT)

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,23 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JEYXGQKYNT"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JEYXGQKYNT');
+            `,
+          }}
+        />
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
